73abee74e377b5d5c7174f48da91393fbf073dec
Adam <35293716+sony671@users.noreply.github.com>
2024-06-22 22:38:20 +0200

    Update SingleInstanceHandler.cs

--- SingleInstanceHandler.cs
+++ SingleInstanceHandler.cs
@@ -6,6 +6,24 @@ using System.Runtime.Serialization.Json;
 using System.Threading;
 
 
+/* 
+ *                  License Apache-2.0
+ * 
+ * Copyright 2024
+ * GitHub: https://github.com/sony671/CSharp-SingleInstanceHandler
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
 namespace SingleInstance
 {
     /// <summary>
